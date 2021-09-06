d5a1b89e144374ff2cf4e217512e9eda566427f7
masamichhhhi <masamichhhi@gmail.com>
2021-09-06 23:09:50 +0900

    add fanIn2

--- fanIn/main.go
+++ fanIn/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	done := make(chan struct{})
@@ -8,7 +11,7 @@ func main() {
 	gen1 := generator(done, 1) // int 1をひたすら送信するチャネル(doneで止める)
 	gen2 := generator(done, 2) // int 2をひたすら送信するチャネル(doneで止める)
 
-	result := fanIn1(done, gen1, gen2) // 1か2を受け取り続けるチャネル
+	result := fanIn2(done, gen1, gen2) // 1か2を受け取り続けるチャネル
 	for i := 0; i < 5; i++ {
 		<-result
 	}
@@ -48,6 +51,38 @@ func fanIn1(done chan struct{}, c1, c2 <-chan int) <-chan int {
 			}
 		}
 	}()
+	return result
+}
+
+func fanIn2(done chan struct{}, cs ...<-chan int) <-chan int {
+	result := make(chan int)
+
+	var wg sync.WaitGroup
+
+	wg.Add(len(cs))
+
+	for i, c := range cs {
+		go func(c <-chan int, i int) {
+			defer wg.Done()
+
+			for num := range c {
+				select {
+				case <-done:
+					fmt.Println("wg.Done", i)
+					return
+				case result <- num:
+					fmt.Println("send", i)
+				}
+			}
+		}(c, i)
+	}
+
+	go func() {
+		wg.Wait()
+		fmt.Println("closing fanin")
+		close(result)
+	}()
+
 	return result
 }
 
